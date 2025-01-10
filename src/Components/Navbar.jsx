b99d1c0fc9e05fb21029087fdd3e29ecd7bc336c
asadishaq15 <asadishaq392@gmail.com>
2025-01-10 22:37:36 +0500

    update button

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import logo from "../assets/sampleLogo.png";
-
+import logo from "../assets/Logo/logo.png";
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -24,62 +23,61 @@ const Navbar = () => {
     }, []);
   
     return (
-      <div className="fixed w-full z-50 flex justify-center items-center px-4 top-6">
+      <div className="fixed w-full z-50 flex justify-center items-center px-4 top-4">
         <nav
           className={`transition-all duration-300 rounded-[30px] max-w-4xl w-full bg-gray-900/10 backdrop-blur-md ${
             isOpen ? "!rounded-[30px]" : "rounded-[30px]"
           }`}
         >
-          <div className="flex justify-between items-center px-6 py-4">
+          <div className="flex justify-between items-center px-4 py-2">
             {/* Logo */}
             <Link to="/" className="flex-shrink-0">
               <span className="text-white font-syne">Logo</span>
             </Link>
   
-
           {/* Desktop Navigation */}
-          <div className="hidden md:flex items-center space-x-6">
+          <div className="hidden md:flex items-center space-x-4">
             <Link
               to="/"
-              className="text-white relative group transition-all duration-500 font-syne text-sm px-6 py-2 rounded-full before:content-[''] before:absolute before:inset-0 before:rounded-full before:transition-all before:duration-300 hover:text-orange-500 before:opacity-0 hover:before:opacity-100 before:bg-white/20"
+              className="text-white relative group transition-all duration-500 font-syne text-base px-4 py-1.5 rounded-full before:content-[''] before:absolute before:inset-0 before:rounded-full before:transition-all before:duration-300 hover:text-orange-500 before:opacity-0 hover:before:opacity-100 before:bg-white/20"
             >
               Home
-              <span className="absolute bottom-0 left-1/2 w-10 h-0.5 bg-orange-500 transform -translate-x-1/2 scale-x-0 group-hover:scale-x-100 transition-transform duration-500"></span>
+              <span className="absolute bottom-0 left-1/2 w-8 h-0.5 bg-orange-500 transform -translate-x-1/2 scale-x-0 group-hover:scale-x-100 transition-transform duration-500"></span>
             </Link>
             <Link
               to="/projects"
-              className="text-white relative group transition-all duration-500 font-syne text-sm px-6 py-2 rounded-full before:content-[''] before:absolute before:inset-0 before:rounded-full before:transition-all before:duration-300 hover:text-orange-500 before:opacity-0 hover:before:opacity-100 before:bg-white/20"
+              className="text-white relative group transition-all duration-500 font-syne text-base px-4 py-1.5 rounded-full before:content-[''] before:absolute before:inset-0 before:rounded-full before:transition-all before:duration-300 hover:text-orange-500 before:opacity-0 hover:before:opacity-100 before:bg-white/20"
             >
               Projects
-              <span className="absolute bottom-0 left-1/2 w-10 h-0.5 bg-orange-500 transform -translate-x-1/2 scale-x-0 group-hover:scale-x-100 transition-transform duration-500"></span>
+              <span className="absolute bottom-0 left-1/2 w-8 h-0.5 bg-orange-500 transform -translate-x-1/2 scale-x-0 group-hover:scale-x-100 transition-transform duration-500"></span>
             </Link>
             <Link
               to="/albums"
-              className="text-white relative group transition-all duration-500 font-syne text-sm px-6 py-2 rounded-full before:content-[''] before:absolute before:inset-0 before:rounded-full before:transition-all before:duration-300 hover:text-orange-500 before:opacity-0 hover:before:opacity-100 before:bg-white/20"
+              className="text-white relative group transition-all duration-500 font-syne text-base px-4 py-1.5 rounded-full before:content-[''] before:absolute before:inset-0 before:rounded-full before:transition-all before:duration-300 hover:text-orange-500 before:opacity-0 hover:before:opacity-100 before:bg-white/20"
             >
               Albums
-              <span className="absolute bottom-0 left-1/2 w-10 h-0.5 bg-orange-500 transform -translate-x-1/2 scale-x-0 group-hover:scale-x-100 transition-transform duration-500"></span>
+              <span className="absolute bottom-0 left-1/2 w-8 h-0.5 bg-orange-500 transform -translate-x-1/2 scale-x-0 group-hover:scale-x-100 transition-transform duration-500"></span>
             </Link>
             <Link
               to="/about"
-              className="text-white relative group transition-all duration-500 font-syne text-sm px-6 py-2 rounded-full before:content-[''] before:absolute before:inset-0 before:rounded-full before:transition-all before:duration-300 hover:text-orange-500 before:opacity-0 hover:before:opacity-100 before:bg-white/20"
+              className="text-white relative group transition-all duration-500 font-syne text-base px-4 py-1.5 rounded-full before:content-[''] before:absolute before:inset-0 before:rounded-full before:transition-all before:duration-300 hover:text-orange-500 before:opacity-0 hover:before:opacity-100 before:bg-white/20"
             >
               About
-              <span className="absolute bottom-0 left-1/2 w-10 h-0.5 bg-orange-500 transform -translate-x-1/2 scale-x-0 group-hover:scale-x-100 transition-transform duration-500"></span>
+              <span className="absolute bottom-0 left-1/2 w-8 h-0.5 bg-orange-500 transform -translate-x-1/2 scale-x-0 group-hover:scale-x-100 transition-transform duration-500"></span>
             </Link>
             <Link
               to="/services"
-              className="text-white relative group transition-all duration-500 font-syne text-sm px-6 py-2 rounded-full before:content-[''] before:absolute before:inset-0 before:rounded-full before:transition-all before:duration-300 hover:text-orange-500 before:opacity-0 hover:before:opacity-100 before:bg-white/20"
+              className="text-white relative group transition-all duration-500 font-syne text-base px-4 py-1.5 rounded-full before:content-[''] before:absolute before:inset-0 before:rounded-full before:transition-all before:duration-300 hover:text-orange-500 before:opacity-0 hover:before:opacity-100 before:bg-white/20"
             >
               Services
-              <span className="absolute bottom-0 left-1/2 w-10 h-0.5 bg-orange-500 transform -translate-x-1/2 scale-x-0 group-hover:scale-x-100 transition-transform duration-500"></span>
+              <span className="absolute bottom-0 left-1/2 w-8 h-0.5 bg-orange-500 transform -translate-x-1/2 scale-x-0 group-hover:scale-x-100 transition-transform duration-500"></span>
             </Link>
             <Link
               to="/blogs"
-              className="text-white relative group transition-all duration-500 font-syne text-sm px-6 py-2 rounded-full before:content-[''] before:absolute before:inset-0 before:rounded-full before:transition-all before:duration-300 hover:text-orange-500 before:opacity-0 hover:before:opacity-100 before:bg-white/20"
+              className="text-white relative group transition-all duration-500 font-syne text-base px-4 py-1.5 rounded-full before:content-[''] before:absolute before:inset-0 before:rounded-full before:transition-all before:duration-300 hover:text-orange-500 before:opacity-0 hover:before:opacity-100 before:bg-white/20"
             >
               Blogs
-              <span className="absolute bottom-0 left-1/2 w-10 h-0.5 bg-orange-500 transform -translate-x-1/2 scale-x-0 group-hover:scale-x-100 transition-transform duration-500"></span>
+              <span className="absolute bottom-0 left-1/2 w-8 h-0.5 bg-orange-500 transform -translate-x-1/2 scale-x-0 group-hover:scale-x-100 transition-transform duration-500"></span>
             </Link>
           </div>
 
@@ -87,7 +85,7 @@ const Navbar = () => {
            <div className="hidden md:block">
             <Link
               to="/contact"
-              className="bg-white text-black px-8 py-3 rounded-full hover:bg-orange-500 hover:text-white transition-colors duration-300 font-syne text-base"
+              className="bg-white text-black px-6 py-2 rounded-full hover:bg-orange-500 hover:text-white transition-colors duration-300 font-syne text-lg"
             >
               Let's Talk
             </Link>
@@ -118,7 +116,7 @@ const Navbar = () => {
           </div>
         </div>
 
-        {/* Mobile Navigation - Updated */}
+        {/* Mobile Navigation */}
         <div
           className={`md:hidden transition-all duration-300 overflow-hidden ${
             isOpen ? "max-h-[400px] opacity-100 py-4" : "max-h-0 opacity-0 py-0"
